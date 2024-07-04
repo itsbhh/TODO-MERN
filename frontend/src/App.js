0843f3c9bd63724bb8b53922488fcd6d0109d08d
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2024-07-04 14:39:01 +0530

    creating UI using react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
+import Footer from "./components/footer/Footer";
+
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Home />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+const Home = () => {
+  return (
+    <div className="home d-flex justify-content-center align-items-center">
+      <div className="container home d-flex justify-content-center align-items-center flex-column">
+        <h1>Effortlessly Organize, <br /> and Enhance Your Life !</h1>
+        <p className="para">Designed to help you organize your life with ease and efficiency,
+        <br/>this brings a touch of elegance to managing your tasks and deadlines.</p> 
+        <button className="home-btn">Create Your List</button></div>
+        
+    </div>
+  );
+};
+
+export default Home;
